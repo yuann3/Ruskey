a295c11f6e9b6abc9b6d28a80e979240846ef579
Yiyuan Li <yy@eyuan.me>
2025-03-08 16:46:12 +0800

    Evaluate Expressions
    Integer Literals
    Boolean Literals
    Null
    Prefix Expressions    Infix Expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,12 +1,42 @@
-use crate::ast::{Expression, ExpressionStatement, IntegerLiteral, Program, Statement};
-use crate::object::{Integer, Null, Object};
+use crate::ast::{
+    self, Expression, ExpressionStatement, InfixExpression, IntegerLiteral, PrefixExpression,
+    Program, Statement,
+};
+use crate::object::{Boolean, Integer, Null, Object, ObjectType};
+use std::sync::OnceLock;
+
+/// Static TRUE and FALSE objects
+fn true_obj() -> &'static Boolean {
+    static TRUE: OnceLock<Boolean> = OnceLock::new();
+    TRUE.get_or_init(|| Boolean::new(true))
+}
+
+fn false_obj() -> &'static Boolean {
+    static FALSE: OnceLock<Boolean> = OnceLock::new();
+    FALSE.get_or_init(|| Boolean::new(false))
+}
+
+/// Null obj
+fn null_obj() -> &'static Null {
+    static NULL: OnceLock<Null> = OnceLock::new();
+    NULL.get_or_init(Null::new)
+}
+
+/// Convert Rust bool to monkey Boolean Obj
+fn native_bool_to_boolean_object(input: bool) -> Box<dyn Object> {
+    if input {
+        Box::new(true_obj().clone())
+    } else {
+        Box::new(false_obj().clone())
+    }
+}
 
 pub fn eval(program: &Program) -> Box<dyn Object> {
     eval_program(program)
 }
 
 fn eval_program(program: &Program) -> Box<dyn Object> {
-    let mut result: Box<dyn Object> = Box::new(Null::new());
+    let mut result: Box<dyn Object> = Box::new(null_obj().clone());
     for statement in &program.statements {
         result = eval_statement(statement.as_ref());
     }
@@ -16,13 +46,115 @@ fn eval_program(program: &Program) -> Box<dyn Object> {
 fn eval_statement(statement: &dyn Statement) -> Box<dyn Object> {
     match statement.as_any().downcast_ref::<ExpressionStatement>() {
         Some(expr_stmt) => eval_expression(expr_stmt.expression.as_ref()),
-        None => Box::new(Null::new()),
+        None => Box::new(null_obj().clone()),
     }
 }
 
 fn eval_expression(expression: &dyn Expression) -> Box<dyn Object> {
-    match expression.as_any().downcast_ref::<IntegerLiteral>() {
-        Some(int_lit) => Box::new(Integer::new(int_lit.value)),
-        None => Box::new(Null::new()),
+    if let Some(int_lit) = expression.as_any().downcast_ref::<IntegerLiteral>() {
+        return Box::new(Integer::new(int_lit.value));
     }
+
+    // Fixed: Use ast::Boolean instead of object::Boolean
+    if let Some(bool_lit) = expression.as_any().downcast_ref::<ast::Boolean>() {
+        return native_bool_to_boolean_object(bool_lit.value);
+    }
+
+    if let Some(prefix) = expression.as_any().downcast_ref::<PrefixExpression>() {
+        let right = eval_expression(prefix.right.as_ref());
+        return eval_prefix_expression(&prefix.operator, right);
+    }
+
+    if let Some(infix) = expression.as_any().downcast_ref::<InfixExpression>() {
+        let left = eval_expression(infix.left.as_ref());
+        let right = eval_expression(infix.right.as_ref());
+        return eval_infix_expression(&infix.operator, left, right);
+    }
+
+    Box::new(null_obj().clone())
+}
+
+fn eval_infix_expression(
+    operator: &str,
+    left: Box<dyn Object>,
+    right: Box<dyn Object>,
+) -> Box<dyn Object> {
+    if left.type_() == ObjectType::Integer && right.type_() == ObjectType::Integer {
+        return eval_integer_infix_expression(operator, left, right);
+    }
+
+    if operator == "==" {
+        let left_is_true = left
+            .as_any()
+            .downcast_ref::<Boolean>()
+            .map_or(false, |b| b.value);
+        let right_is_true = right
+            .as_any()
+            .downcast_ref::<Boolean>()
+            .map_or(false, |b| b.value);
+        return native_bool_to_boolean_object(left_is_true == right_is_true);
+    }
+
+    if operator == "!=" {
+        let left_is_true = left
+            .as_any()
+            .downcast_ref::<Boolean>()
+            .map_or(false, |b| b.value);
+        let right_is_true = right
+            .as_any()
+            .downcast_ref::<Boolean>()
+            .map_or(false, |b| b.value);
+        return native_bool_to_boolean_object(left_is_true != right_is_true);
+    }
+
+    Box::new(null_obj().clone())
+}
+
+fn eval_integer_infix_expression(
+    operator: &str,
+    left: Box<dyn Object>,
+    right: Box<dyn Object>,
+) -> Box<dyn Object> {
+    let left_val = left.as_any().downcast_ref::<Integer>().unwrap().value;
+    let right_val = right.as_any().downcast_ref::<Integer>().unwrap().value;
+
+    match operator {
+        "+" => Box::new(Integer::new(left_val + right_val)),
+        "-" => Box::new(Integer::new(left_val - right_val)),
+        "*" => Box::new(Integer::new(left_val * right_val)),
+        "/" => Box::new(Integer::new(left_val / right_val)),
+        "<" => native_bool_to_boolean_object(left_val < right_val),
+        ">" => native_bool_to_boolean_object(left_val > right_val),
+        "==" => native_bool_to_boolean_object(left_val == right_val),
+        "!=" => native_bool_to_boolean_object(left_val != right_val),
+        _ => Box::new(null_obj().clone()), // The book would return an error here
+    }
+}
+
+fn eval_prefix_expression(operator: &str, right: Box<dyn Object>) -> Box<dyn Object> {
+    match operator {
+        "!" => eval_bang_operator_expression(right),
+        "-" => eval_minus_prefix_operator_expression(right),
+        _ => Box::new(null_obj().clone()),
+    }
+}
+
+fn eval_bang_operator_expression(right: Box<dyn Object>) -> Box<dyn Object> {
+    if let Some(boolean) = right.as_any().downcast_ref::<Boolean>() {
+        return native_bool_to_boolean_object(!boolean.value);
+    }
+
+    if right.as_any().downcast_ref::<Null>().is_some() {
+        return native_bool_to_boolean_object(true);
+    }
+
+    native_bool_to_boolean_object(false)
+}
+
+fn eval_minus_prefix_operator_expression(right: Box<dyn Object>) -> Box<dyn Object> {
+    if let Some(integer) = right.as_any().downcast_ref::<Integer>() {
+        return Box::new(Integer::new(-integer.value));
+    }
+
+    Box::new(null_obj().clone())
 }
--- tests/evaluator_tests.rs
+++ tests/evaluator_tests.rs
@@ -1,6 +1,6 @@
 use ruskey::evaluator::eval;
 use ruskey::lexer::Lexer;
-use ruskey::object::{Integer, Object};
+use ruskey::object::{Boolean, Integer, Object};
 use ruskey::parser::Parser;
 
 #[test]
@@ -20,7 +20,84 @@ fn test_eval(input: &str) -> Box<dyn Object> {
 }
 
 fn test_integer_object(obj: &dyn Object, expected: i64) {
-    // We need to downcast from &dyn Object to &Integer
     let integer = obj.as_any().downcast_ref::<Integer>().unwrap();
     assert_eq!(integer.value, expected, "object has wrong value");
+}
+
+#[test]
+fn test_eval_boolean_expression() {
+    let tests = vec![("true", true), ("false", false)];
+
+    for (input, expected) in tests {
+        let evaluated = test_eval(input);
+        test_boolean_object(evaluated.as_ref(), expected);
+    }
+}
+
+fn test_boolean_object(obj: &dyn Object, expected: bool) {
+    let boolean = obj.as_any().downcast_ref::<Boolean>().unwrap();
+    assert_eq!(
+        boolean.value, expected,
+        "object has wrong value. got={}, want={}",
+        boolean.value, expected
+    );
+}
+
+#[test]
+fn test_bang_operator() {
+    let tests = vec![
+        ("!true", false),
+        ("!false", true),
+        ("!5", false),
+        ("!!true", true),
+        ("!!false", false),
+        ("!!5", true),
+    ];
+
+    for (input, expected) in tests {
+        let evaluated = test_eval(input);
+        test_boolean_object(evaluated.as_ref(), expected);
+    }
+}
+
+#[test]
+fn test_minus_operator() {
+    let tests = vec![("-5", -5), ("-10", -10), ("--5", 5)];
+
+    for (input, expected) in tests {
+        let evaluated = test_eval(input);
+        test_integer_object(evaluated.as_ref(), expected);
+    }
+}
+
+#[test]
+fn test_eval_integer_infix_expressions() {
+    enum Expected {
+        Int(i64),
+        Bool(bool),
+    }
+
+    let tests = vec![
+        ("5 + 5", Expected::Int(10)),
+        ("5 - 5", Expected::Int(0)),
+        ("5 * 5", Expected::Int(25)),
+        ("5 / 5", Expected::Int(1)),
+        ("5 > 5", Expected::Bool(false)),
+        ("5 < 5", Expected::Bool(false)),
+        ("5 == 5", Expected::Bool(true)),
+        ("5 != 5", Expected::Bool(false)),
+        ("5 > 4", Expected::Bool(true)),
+        ("5 < 6", Expected::Bool(true)),
+        ("5 == 6", Expected::Bool(false)),
+        ("5 != 6", Expected::Bool(true)),
+    ];
+
+    for (input, expected) in tests {
+        let evaluated = test_eval(input);
+
+        match expected {
+            Expected::Int(int) => test_integer_object(evaluated.as_ref(), int),
+            Expected::Bool(bool_val) => test_boolean_object(evaluated.as_ref(), bool_val),
+        }
+    }
 }
