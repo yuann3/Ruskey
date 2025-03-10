f5b46f253d849dadedf2c79c420d566b84c036f8
Yiyuan Li <yy@eyuan.me>
2025-03-10 15:43:09 +0800

    remove old repl test

--- tests/repl_tests.rs
+++ tests/repl_tests.rs
@@ -2,25 +2,6 @@ use ruskey::repl::Repl;
 use std::io::Cursor;
 
 #[test]
-fn test_repl_basic_functionality() {
-    let mut input = "let x = 5;\n".as_bytes();
-    let mut output = Vec::new();
-
-    let mut repl = Repl::new();
-    repl.start(&mut input, &mut output).unwrap();
-
-    let output_str = String::from_utf8(output).unwrap();
-
-    assert!(output_str.contains("Type:Let"));
-    assert!(output_str.contains("Literal:let"));
-    assert!(output_str.contains("Type:Ident"));
-    assert!(output_str.contains("Literal:x"));
-    assert!(output_str.contains("Type:Assign"));
-    assert!(output_str.contains("Type:Int"));
-    assert!(output_str.contains("Literal:5"));
-}
-
-#[test]
 fn test_repl_parser_functionality() {
     let input = "let x = 5 + 5;\n".as_bytes();
     let mut output = Vec::new();
